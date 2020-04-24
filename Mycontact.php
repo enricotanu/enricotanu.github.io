331a25032cd18d8d9be01646925dab568e8be26d
enricotanu <62689923+enricotanu@users.noreply.github.com>
2020-04-24 14:30:11 +0700

    Add files via upload

---
+++ Mycontact.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="project.css">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="interest.html">My interest</a></li>
+                <li><a href="Myproject.html">My project</a></li>
+                <li><a href="Mygallery.html">My Gallery</a></li>
+                <li><a href="Mycontact.html">My Contact</a></li>
+            </ul>
+          </nav>
+          <a href="http://line.me/ti/p/~enrico.tanu"><img class="line" src="Line.png"></a>
+          <a href="https://www.instagram.com/enrico.tanu/"><img class="ig" src="ig.jfif"></a>
+          <a href="mailto: enricotanu01@student.ciputra.ac.id"><img class="mail" src="email.png"></a>
+          <a href="https://api.whatsapp.com/send?phone=6282165119875&text=&source=&data="><img class="wa" src="wat.jfif"></a>
+          <div class="Contact">
+              <h3>Line Id : enrico.tanu</h3>
+              <h3>IG : enrico.tanu</h3>
+              <h3>Email : enricotanu01@ciputra.ac.id</h3>
+              <h3>WA : +6282165119875</h3>
+            </div>
+            <div class="form">
+                <form action="">
+                    <p class="email">E-mail:</p>
+                    <input class="textboxemail" type="text" name="mail"><br>
+                    <p class="email">Comment:</p>
+                    <textarea class="textboxcom"></textarea><br><br>
+                    <input class="buttonproject" type="submit" value="Send">
+                    <input class="buttonproject" type="reset" value="Reset">
+                </form>
+            </div>
+    </body>
+</html>
---
+++ Myproject.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="project.css">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="interest.html">My interest</a></li>
+                <li><a href="Myproject.html">My project</a></li>
+                <li><a href="Mygallery.html">My Gallery</a></li>
+                <li><a href="Mycontact.html">My Contact</a></li>
+            </ul>
+          </nav>
+          <div class="project">
+              <img src="Screenshot (58).png"><h1>Project Algorithm Programing Semester1</h1></img>
+          </div>
+          <h3 class="desc">Ini adalah project yang saya dan teman saya buat untuk UAS Semester1. Ini adalah game pokemon dimana kita akan memilih 6 dari 72 pokemon yang tersedia setelah memilih kita akan menggunakan pokemon tersebut untuk melawan bot dan pertandingan akan berakhir setelah kita dapat mengalahkan bot tersebut.</h3>
+    </body>
+</html>
